6a4a1009040342d3d93dc4f7223e755def48ec71
Iswardahal321 <junmain91@gmail.com>
2025-07-11 12:24:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.js
 import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
@@ -8,7 +9,6 @@ import {
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase/config";
 
-
 // Pages
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
@@ -18,7 +18,7 @@ import JoinTournament from "./pages/JoinTournament.jsx";
 import AdminPanel from "./pages/AdminPanel.jsx";
 import AdminTeams from "./pages/AdminTeams.jsx";
 import SlotList from "./pages/SlotList.jsx";
-import AddSlot from "./pages/AddSlot.jsx"; // ✅ NEW IMPORT
+import AddSlot from "./pages/AddSlot.jsx";
 
 // Components
 import PrivateRoute from "./components/PrivateRoute.jsx";
@@ -87,14 +87,12 @@ function App() {
           }
         />
 
-        {/* Admin Protected Routes */}
+        {/* ✅ Admin Protected Routes - WITHOUT Layout */}
         <Route
           path="/admin"
           element={
             <AdminRoute>
-              <Layout>
-                <AdminPanel />
-              </Layout>
+              <AdminPanel />
             </AdminRoute>
           }
         />
@@ -102,9 +100,7 @@ function App() {
           path="/admin/teams"
           element={
             <AdminRoute>
-              <Layout>
-                <AdminTeams />
-              </Layout>
+              <AdminTeams />
             </AdminRoute>
           }
         />
@@ -112,9 +108,7 @@ function App() {
           path="/admin/add-slot"
           element={
             <AdminRoute>
-              <Layout>
-                <AddSlot />
-              </Layout>
+              <AddSlot />
             </AdminRoute>
           }
         />
