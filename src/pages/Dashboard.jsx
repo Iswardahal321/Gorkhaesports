f71e22c7e9edd6d13f992888347a891080b898c7
Iswardahal321 <junmain91@gmail.com>
2025-07-11 20:10:26 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -40,6 +40,7 @@ const Dashboard = () => {
     );
   };
 
+  
   return (
     <div className="p-4 w-full bg-gray-100 min-h-screen">
       <h1 className="text-3xl font-bold text-center mb-6">🎯 Live Tournaments</h1>
