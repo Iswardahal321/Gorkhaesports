c91bc9943ca9ffcec886649e4433d654b53bb7f3
Iswardahal321 <junmain91@gmail.com>
2025-07-11 17:35:45 +0530

    Update AdminLayout.jsx

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -1,11 +1,22 @@
 // src/components/AdminLayout.jsx
 import React from "react";
 import AdminBottomNav from "./AdminBottomNav";
+import AdminProfileMenu from "./AdminProfileMenu";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/config";
 
 function AdminLayout({ children }) {
+  const [user] = useAuthState(auth);
+
   return (
-    <div className="pb-16">
-      {children}
+    <div className="min-h-screen relative pb-16 bg-gray-50">
+      {/* ✅ Top-right admin profile circle */}
+      <AdminProfileMenu user={user} />
+
+      {/* Main content */}
+      <div className="pt-4 px-4">{children}</div>
+
+      {/* Bottom nav */}
       <AdminBottomNav />
     </div>
   );
