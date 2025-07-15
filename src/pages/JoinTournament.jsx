f895448d946b038d6f7b55740fc154140f6883f6
Iswardahal321 <junmain91@gmail.com>
2025-07-15 09:56:25 +0530

    Update JoinTournament.jsx

--- src/pages/JoinTournament.jsx
+++ src/pages/JoinTournament.jsx
@@ -15,6 +15,7 @@ import {
 import { db, auth } from "../firebase/config";
 import { loadScript } from "../utils/loadScript";
 
+
 const JoinTournament = () => {
   const { id } = useParams();
   const navigate = useNavigate();
